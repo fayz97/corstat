6be100b5d38815f0a72c72db8861da349e50510c
Faizal Ardian Putra <faizal@trutrip.co>
2020-03-17 22:45:11 +0700

    setup api corona virus helper

--- lib/services/coronavirus_tracker.dart
+++ lib/services/coronavirus_tracker.dart
@@ -10,12 +10,39 @@ class CoronaVirusTracker {
     final String fullEndpoint = baseUrl + endpoint;
     Response response = await get(fullEndpoint);
     
+    return processResponse(response);
+  }
+
+  static Future<Map> getConfirmed() async {
+    final String endpoint = '/confirmed';
+    final String fullEndpoint = baseUrl + endpoint;
+    Response response = await get(fullEndpoint);
+
+    return processResponse(response);
+  }
+
+  static Future<Map> getDeaths() async {
+    final String endpoint = '/deaths';
+    final String fullEndpoint = baseUrl + endpoint;
+    Response response = await get(fullEndpoint);
+
+    return processResponse(response);
+  }
+
+  static Future<Map> getRecovered() async {
+    final String endpoint = '/recovered';
+    final String fullEndpoint = baseUrl + endpoint;
+    Response response = await get(fullEndpoint);
+
+    return processResponse(response);
+  }
+
+  static Map processResponse(Response response) {
     int statusCode = response.statusCode;
     if (statusCode == 200) {
       Map data = jsonDecode(response.body);
 
       return data;
     }
-
   }
 }
